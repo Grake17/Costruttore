0a25dab0f4f6f639a320d798743a0bc45cb94e85
Grake17 <62191096+Grake17@users.noreply.github.com>
2020-04-25 00:03:16 +0200

    00:03 25/04/2020 Giulio

--- index.js
+++ index.js
@@ -14,20 +14,17 @@ const insediamenti = require("./insediamenti/insediamenti")
 const fazioni = require("./fazioni/fazioni")
 
 client.on("voiceStateUpdate", (oldMember,newMember) => {
-    if(newMember.channel != null){
-        if(newMember.channel.parent.id == config.parente1){
-            insediamenti(oldMember,newMember,client)
+    if(newMember.channel != null){   
+
+            insediamenti(oldMember,newMember,client)        
             fazioni(oldMember,newMember,client)
-        }else if(newMember.channel.parent.id == config.parente2){
-            insediamenti(oldMember,newMember,client)
-            fazioni(oldMember,newMember,client)
-        }
-    }
-    if(oldMember.channel != null){
+
+         
+    }else if(oldMember.channel != null){
         if(oldMember.channel.parent.id == config.parente1){
             insediamenti(oldMember,newMember,client)
         }else if(oldMember.channel.parent.id == config.parente2){
             fazioni(oldMember,newMember,client)
-        }
+        }        
     }     
 })
